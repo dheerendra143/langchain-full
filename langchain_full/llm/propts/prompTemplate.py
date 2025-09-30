97d93fa6effcdf3ad4da343e0980ffa722b243f6
dheerendra143 <singhdheerendra83@gmail.com>
2025-09-30 15:50:07 +0530

    improve the prompt

--- langchain_full/llm/propts/prompTemplate.py
+++ langchain_full/llm/propts/prompTemplate.py
@@ -4,5 +4,6 @@ template = """you are an expert in traditional cuisines.
         answer : I dont know.
         Avoid giving information about fictional languages. If the language is fictional or non-existent
         answer : I dont know the language.
-        Answer the question: What is the traditional cuisine of {country}?
+        Answer the question: What is the traditional cuisine of {country}? 
+        Answer in {no_of_paras} shorts paras in {language}.
         """
--- langchain_full/prompt_template.py
+++ langchain_full/prompt_template.py
@@ -1,24 +1,38 @@
+from email.policy import default
+
 import streamlit as st
 from langchain_core.prompts import PromptTemplate, ChatPromptTemplate
 
 import llm.config.code_agent_config as chat
-import llm.propts.codeAgentPrompt as code_promp
+import llm.propts.prompTemplate as code_promp
 
-
-st.title("Cuisine assistance")
-query = st.text_input(
-    label="Enter country name",
-    placeholder="Enter country name",
-    )
 
 prompt_template = PromptTemplate(
-    input_variables=["country"],
+    input_variables=["country", "on_of_paras", "language"],
     template=code_promp.template
 )
 
-if query:
+st.title("Cuisine assistance")
+country = st.text_input(
+    label="Enter country name",
+    placeholder="Enter country name"
+    )
+
+no_of_paras = st.number_input(
+    label="Enter number of paras",
+    min_value=1,
+    max_value=15
+)
+
+language = st.text_input(
+    label="Enter language",
+    placeholder="Enter language"
+    )
+
+
+if language:
     # resource = chat.chain.invoke(query)
     # st.write(resource.content)
-    resource = chat.llm.stream(prompt_template.format(country=query))
-    with st.container(height=300):  # Adjust height as needed
-        st.write_stream(resource)
+    resource = chat.llm.stream(prompt_template.format(country=country, no_of_paras=no_of_paras, language=language))
+    # with st.container(height=300):  # Adjust height as needed
+    st.write_stream(resource)
