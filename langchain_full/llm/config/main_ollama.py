5d5106124286ab2442e175f75ea7bbad63fce5b8
dheerendra143 <singhdheerendra83@gmail.com>
2025-09-30 20:55:02 +0530

    Added llama model with travel agent

---
+++ langchain_full/llm/config/main_ollama.py
@@ -0,0 +1,13 @@
+from langchain_community.chat_models import ChatOllama
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate
+
+llm = ChatOllama(model="llama3.2")
+
+# prompt = ChatPromptTemplate.from_messages([
+#     ("system", "{prompt}"),
+#     ("user", "{input}")
+# ])
+# output_parser = StrOutputParser()
+
+chain = llm
---
+++ langchain_full/travel_guide_llama.py
@@ -0,0 +1,42 @@
+from email.policy import default
+
+import streamlit as st
+from langchain_core.prompts import PromptTemplate, ChatPromptTemplate
+
+import llm.config.main_ollama as chat
+import llm.propts.travelGuide as code_prompt
+
+
+prompt_template = PromptTemplate(
+    input_variables=["city", "month", "language", "budget"],
+    template=code_prompt.template
+)
+
+st.title("Travel Guide assistance")
+
+city = st.text_input(
+    label="Enter city",
+    placeholder="Enter city"
+    )
+
+month = st.text_input(
+    label="Enter month",
+    placeholder="Enter month"
+    )
+
+language = st.text_input(
+    label="Enter language",
+    placeholder="Enter language"
+    )
+
+budget = st.selectbox("Travel Budget", ["low", "medium", "high"])
+
+
+if city and month and language and budget:
+    # resource = chat.chain.invoke(query)
+    # st.write(resource.content)
+    input_prompt = prompt_template.format(city=city, month=month, language=language, budget=budget)
+    print(input_prompt)
+    resource = chat.llm.stream(input_prompt)
+    # with st.container(height=300):  # Adjust height as needed
+    st.write_stream(resource)
