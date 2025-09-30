31d5fe5ba755c72cadad553c887fc9226cc6187e
dheerendra143 <singhdheerendra83@gmail.com>
2025-09-30 21:07:30 +0530

    Added llama model with travel agent

--- langchain_full/llm/propts/travelGuide.py
+++ langchain_full/llm/propts/travelGuide.py
@@ -5,5 +5,8 @@ template = """ You arte travel guide and you will use {language} to share all be
     2. Local cuisines you must try.
     3. Useful phrases in {language}.
     4. Tips for traveling on a {budget} budget.
+    5. Share the important contact details in case of emergencies.
+    6. {budget} budget hotels details with its address details, contact details, and map link to stay.
+    7. Transportation details in {budget} budget.
     Enjoy your trip!
 """
