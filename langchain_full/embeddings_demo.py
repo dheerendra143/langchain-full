e8357dbe12acbfccae73234e693fbacb11ad2813
dheerendra143 <singhdheerendra83@gmail.com>
2025-10-06 19:13:26 +0530

    embedding

--- langchain_full/embeddings_demo.py
+++ langchain_full/embeddings_demo.py
@@ -3,16 +3,25 @@
 import streamlit as st
 from langchain_core.prompts import PromptTemplate, ChatPromptTemplate
 import llm.config.openai_embedding as chat
-
+import numpy as np
 
 st.title("Speech Generator")
 
 question = st.text_input(
-    label="Enter question",
+    label="Enter question1",
+)
+
+question2 = st.text_input(
+    label="Enter question2",
 )
 
 
-if question:
+
+if question and question2:
     resource = chat.llm.embed_query(question)
-    print(resource)
-    st.write_stream(resource)
+    resource2 = chat.llm.embed_query(question2)
+
+    similarity_score = np.dot(resource, resource2)
+
+    print(similarity_score * 100, '%')
+    # st.write_stream(resource)
